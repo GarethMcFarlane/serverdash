c4881195c963e4cfa69ffc27fa0d746cc3c33cab
Gareth McFarlane <gmcf8558@uni.sydney.edu.au>
2016-03-08 21:17:09 +1100

    New powershell script for logs

--- REST.ps1
+++ REST.ps1
@@ -14,6 +14,8 @@ $result = 'No backup configured'
 $lastBackup = 'Never'
 $lastSummary = Get-WBSummary
 $backupCopies = 0
+$today = Get-Date
+$yesterday = $today.AddDays(-1)
 
 
  # Checks that a scheduled backup exists
@@ -37,3 +39,30 @@ $uri = "https://boiling-fire-537.firebaseio.com/servers/" + $domainName + "/" + 
 #Convert to JSON and send.
 $json = $server | ConvertTo-Json
 $response = Invoke-RestMethod $uri -Method Put -Body $json -ContentType 'application/json'
+
+
+
+$logs = @("Application", "System")
+$types = @("Warning", "Critical","Error")
+
+foreach ($log in $logs){
+	foreach ($type in $types){
+	
+		$errors = New-Object PSObject
+		
+		$events = get-eventlog -logname $log -after $yesterday | Where {$_.entryType -Match $type}
+		$table = $events | Group EventID, Source
+	
+		foreach($row in $table) {
+			Add-Member -InputObject $errors -MemberType NoteProperty -Name $row.Name -Value $row.Count
+		}
+	
+
+$uri = "https://boiling-fire-537.firebaseio.com/servers/" + $domainName + "/" + $computerName + "/logs/" + $log + "/" + $type + ".json"
+#Convert to JSON and send.
+$json = $errors | ConvertTo-Json
+$response = Invoke-RestMethod $uri -Method Put -Body $json -ContentType 'application/json'
+	
+	}
+}
+	
